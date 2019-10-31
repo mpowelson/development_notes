de69565af6c9b796530588a643b3025749447dcf
Matthew Powelson <matthew.powelson@swri.org>
2019-10-31 09:27:24 -0500

    C++ Code Sample: Comparing lambda vs std bind with std::function

---
+++ Cpp_Code_Samples/lambda_vs_std_bind.cpp
@@ -0,0 +1,39 @@
+﻿#include <functional>
+#include <iostream>
+#include <eigen3/Eigen>
+
+class Bar
+{
+public:
+    Bar(std::function<int(int)> func) : func_(func)
+    {
+        int i = func_(5);
+        // std::cout << "Bar Constructor: " <<  i  << std::endl;
+    }
+    std::function<int(int)> func_;
+
+};
+
+class Foo
+{
+private:
+    Bar bar_object_;
+
+public:
+    int callback(int input) {return input + 1;}
+
+    // Foo() : bar_object_(std::bind(&Foo::callback, this, std::placeholders::_1)) {}
+     Foo() : bar_object_([&](int x){return Foo::callback(x);}) {}
+
+
+
+};
+
+
+
+
+int main(int argc, char** argv)
+{
+  Foo test;
+  return 0;
+}
